68569a0166d78d73a3abe890cb8565ac9787ad13
平島栄志 <takashi@Hirashima-MacBook-Pro.local>
2018-07-07 19:54:40 +0900

    create my first react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { FormApp } from './FormApp';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> This is my first app! </h1>
+        <FormApp />
+      </div>
+    );
+  }
+}
+
+export default App;
